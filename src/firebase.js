f1e1139869ec1fba4901f0590f28823775d6f23d
Leos <highwind26@gmail.com>
2022-06-13 23:53:50 +0900

    firebase setup [ End Chapter 2 ]

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyClOTKKg9cwy2tZtxKsTJzHT9yW0sT0x_E",
+  authDomain: "leowitter-f9d00.firebaseapp.com",
+  projectId: "leowitter-f9d00",
+  storageBucket: "leowitter-f9d00.appspot.com",
+  messagingSenderId: "313340143948",
+  appId: "1:313340143948:web:3ce9ed0a1ef5625da547e9"
+};
+
+export default initializeApp(firebaseConfig);
